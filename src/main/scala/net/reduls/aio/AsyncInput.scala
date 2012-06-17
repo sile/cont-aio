3d82b34bf6caad92c42c75c02eb142b2ce3bcf40
Takeru Ohta <phjgt308@gmail.com>
2012-06-17 18:21:09 +0900

    implement AsyncInput class

--- src/main/scala/net/reduls/aio/AsyncInput.scala
+++ src/main/scala/net/reduls/aio/AsyncInput.scala
@@ -1,8 +1,7 @@
 package net.reduls.aio
 
-import scala.util.continuations._
-import scala.actors._
-import scala.actors.Actor._
+import scala.util.continuations.shift
+import scala.actors.Actor.actor
 import java.io.InputStream
 
 trait AsyncInput extends InputStream {
@@ -17,4 +16,46 @@ trait AsyncInput extends InputStream {
       }   
     }
   }
+
+  def asyncRead(bytes:Array[Byte]) = {
+    if(available != 0) {
+      read(bytes)
+    } else {
+      shift {ctx: (Int => Unit) =>
+        actor{
+          ctx(read(bytes))
+        }:Unit
+      }
+    }
+  }
+  
+  def asyncRead(bytes:Array[Byte], offset:Int, length:Int) = {
+    if(available != 0) {
+      read(bytes)
+    } else {
+      shift {ctx: (Int => Unit) =>
+        actor{
+          ctx(read(bytes, offset, length))
+        }:Unit
+      }
+    }
+  }
 }
+
+/*
+ [sample code]
+ import java.io._
+ import net.reduls.aio._
+ import scala.util.continuations._
+
+ val buf = new Array[Byte](10)
+ val in = new BufferedInputStream(System.in) with AsyncInput
+
+ println("before reset")
+ reset {
+   val len = in.asyncRead(buf)
+   println("len: "+len)
+   println("bytes: " + buf)
+ }
+ println("after reset")
+ */
