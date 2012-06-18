540e34438feb7fbc45b161eb3926ee751c12c055
Takeru Ohta <phjgt308@gmail.com>
2012-06-18 13:57:50 +0900

    HttpServer実装開始

--- src/main/scala/net/reduls/aio/sample/httpserver/HttpServer.scala
+++ src/main/scala/net/reduls/aio/sample/httpserver/HttpServer.scala
@@ -1,5 +1,20 @@
 package net.reduls.aio.sample.httpserver
 
+import java.net.{ServerSocket,Socket}
+
 class HttpServer {
+  def startSync(port:Int) {
+    val serverSocket = new ServerSocket(port)
+    val clientSocket = serverSocket.accept()
+    // TODO:
+  }
+
+  def startAsync(port:Int) {
+    val serverSocket = new ServerSocket(port)
+    val clientSocket = serverSocket.accept()
+    // TODO:    
+  }
   
+  def registerAction(mappingPathRegex:String, action:HttpAction) {
+  }
 }
