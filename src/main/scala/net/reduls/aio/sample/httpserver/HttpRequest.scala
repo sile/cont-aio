a937b86b2e91c7328caa3e06e3ea890b393bed97
Takeru Ohta <phjgt308@gmail.com>
2012-06-18 19:43:13 +0900

    HTTPリクエストパーサ実装中

--- src/main/scala/net/reduls/aio/sample/httpserver/HttpRequest.scala
+++ src/main/scala/net/reduls/aio/sample/httpserver/HttpRequest.scala
@@ -1,5 +1,39 @@
 package net.reduls.aio.sample.httpserver
 
-class HttpRequest {
-  
+import java.io.InputStream
+
+class HttpRequest(val method:String) {
+//  val path
+//  val params
+//  val headers
+}
+
+object HttpRequest {
+  def apply(in:InputStream):HttpRequest = {
+    val (method,url,version) = parseRequestLine(in)
+    new HttpRequest(method)
+  }
+
+  def parseRequestLine(in:InputStream) = {
+    val line = readLine(in)
+    val regexp = "^([^ ]+) +([^ ]+) +([^ ]+)$".r;
+    line match { 
+      case regexp(method, path, version)  => (method, path, version)
+      case _ => throw new Exception("Invalid http request line: " + line)
+    }
+  }
+
+  def readLine(in:InputStream):String = {
+    val sb = new StringBuilder
+    def recur (pred:Int):String = {
+      val c = in.read
+      if(pred == -1 || (pred == '\r' && c == '\n')) {
+        sb.toString
+      } else {
+        sb.append(pred.toChar)
+        recur(c)
+      } 
+    }
+    recur(in.read)
+  }
 }
--- src/main/scala/net/reduls/aio/sample/httpserver/HttpServer.scala
+++ src/main/scala/net/reduls/aio/sample/httpserver/HttpServer.scala
@@ -5,8 +5,16 @@ import java.net.{ServerSocket,Socket}
 class HttpServer {
   def startSync(port:Int) {
     val serverSocket = new ServerSocket(port)
-    val clientSocket = serverSocket.accept()
-    // TODO:
+    println("start: " + serverSocket)
+    
+//    while(true) {
+      val clientSocket = serverSocket.accept()
+      println("accept: " + clientSocket)
+
+      val request = HttpRequest(clientSocket.getInputStream)
+    println(request)
+      // TODO:
+//    }
   }
 
   def startAsync(port:Int) {
